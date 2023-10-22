88047edd1456600e1ec65e92e7f9181392b514fd
martingrgv <martin.geor04@gmail.com>
2023-10-23 00:53:30 +0300

    first commit

---
+++ Shapes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Shapes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Shape> shapes = new List<Shape>();       
+
+        Circle cir = new Circle(){ Radius = 5 };
+        Triangle tri = new Triangle(){ Base = 5, Height = 3 };
+        Square sqr = new Square(){ Side = 5 };
+        Rectangle rec = new Rectangle(){ Width = 5, Height = 3 };
+        Parallelogram par = new Parallelogram(){ Base = 5, Height = 3};
+        Rhombus rh = new Rhombus(){ Diagonal1 = 5, Diagonal2 = 3 };
+        Trapezoid trap = new Trapezoid(){ BaseA = 5, BaseB = 3, Height = 2 };
+
+        shapes.Add(cir);
+        shapes.Add(tri);
+        shapes.Add(sqr);
+        shapes.Add(rec);
+        shapes.Add(par);
+        shapes.Add(rh);
+        shapes.Add(trap);
+
+        foreach (var shape in shapes)
+        {
+            System.Console.WriteLine(Math.Round(shape.CalculateArea(), 2));
+        }
+    }
+}
---
+++ Shapes/Shapes/Shape.cs
@@ -0,0 +1,91 @@
+﻿namespace Shapes;
+
+public class Shape
+{
+    public virtual double CalculateArea()
+    {
+        return 0;
+    }
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * (Radius * Radius);
+    }
+}
+
+public class Triangle : Shape
+{
+    public double Base { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return 0.5 * Base * Height;
+    }
+}
+
+public class Square : Shape
+{
+    public double Side { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Side * Side;
+    }
+}
+ public class Rectangle : Shape
+ {
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Width * Height;
+    }
+ }
+
+ public class Parallelogram : Shape
+ {
+    public double Base { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Base * Height;
+    }
+}
+
+public class Rhombus : Shape
+{
+    public double Diagonal1 { get; set; }
+    public double Diagonal2 { get; set; }
+    public double Base { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        if (Diagonal1 != 0 && Diagonal2 != 0)
+        {
+            return 05 * Diagonal1 * Diagonal2;
+        }
+
+        return Base * Height;
+    }
+}
+
+ public class Trapezoid : Shape
+ {
+    public double BaseA { get; set; }
+    public double BaseB { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return (BaseA + BaseB) / 2 * Height;
+    }
+}
